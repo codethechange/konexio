35ee0b2021193d084d2bbdfc6d9c2dc00b766406
Zage Strassberg-Phillips <zagephillips@gmail.com>
2018-03-09 17:32:06 -0800

    hello konexio

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const hbs = require("hbs");
+//comes with node
+const path = require("path");
+const axios = require("axios");
+
+// const port = process.env.PORT || 3000;
+
+const app = express();
+
+app.set("views", path.join(__dirname, "views"));
+//default hbs file
+app.set("view engine", "hbs");
+
+app.use(express.static(path.join(__dirname, "public")))
+
+app.get("/", (req, res) => {
+  res.render("index.hbs");
+})
+
+app.listen(3000, () => {
+  console.log("Server up on port 3000");
+})
