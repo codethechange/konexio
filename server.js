fd956df17af8b32ce24caa8cdc3f98ac00adcf2b
Zage Strassberg-Phillips <zagephillips@gmail.com>
2018-04-13 17:07:28 -0700

    heroku stuff

--- server.js
+++ server.js
@@ -2,23 +2,11 @@ const express = require("express");
 const hbs = require("hbs");
 //comes with node
 const path = require("path");
-const axios = require("axios");
 //for post
 const bodyParser = require("body-parser");
-/**bodyParser.json(options)
- * Parses the text as JSON and exposes the resulting object on req.body.
- */
-// app.use(bodyParser.json());
-/** bodyParser.urlencoded(options)
- * Parses the text as URL encoded data (which is how browsers tend to send form data from regular forms set to POST)
- * and exposes the resulting object (containing the keys and values) on req.body
- */
-// app.use(bodyParser.urlencoded({
-//     extended: true
-// }));
+const mongoose = require("mongoose");
 
 const port = process.env.PORT || 3000;
-
 const app = express();
 
 // app.use(bodyParser.json());
@@ -37,21 +25,12 @@ app.get("/", (req, res) => {
   res.render("main-page.hbs");
 })
 
-// app.get("/profile-page", (req, res) => {
-//   res.render("profile-page.hbs");
-// })
-//
-app.get("/form.hbs", (req, res) => {
-  console.log("Hello");
-  res.render("form.hbs");
+app.get("/form/new", (req, res) => {
+  res.render("newForm.hbs");
 })
 
-app.post("/form.hbs", (req, res) => {
-    // console.log(req.body);
-    // console.log("HELLO");
-    const body = req.body;
-    console.log(bodyParser);
-    res.send(`You sent: ${body} to Express`);
+app.post("/form/new", (req, res) => {
+    res.send(req.body);
 });
 
 app.listen(port, () => {
