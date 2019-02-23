e4031d741303ae312c1d43c46cceee11e275e236
Anthony Perez <anthonyacostaperez@gmail.com>
2019-02-23 15:04:58 -0800

    Add files via upload

---
+++ src/components/WelcomeButton.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import '../stylesheets/WelcomeButton.css';
+
+export class WelcomeButton extends Component {
+    render() {
+        return (
+            <React.Fragment>
+            <div className="mentor-container">
+               <Link to="/MentorSignUp">
+                    <button className="mentor-button">
+                        <span className="mentor-button-text">I am a mentor</span>
+                    </button>
+                </Link>
+            </div>
+            <div className="mentee-container">
+               <Link to="/MenteeSignUp">
+                    <button className="mentee-button">
+                        <span className="mentee-button-text">I am looking for a mentor</span>
+                    </button>
+                </Link>
+            </div>
+            </ React.Fragment>
+        );
+    }
+};
