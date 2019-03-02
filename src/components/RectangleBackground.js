2f7d4fa31ea76b7b560c96a90765fe3c744dcd0d
Eric Zhou <ericzhou@stanford.edu>
2019-03-02 12:34:52 -0800

    Reformated things

--- src/components/RectangleBackground.js
+++ src/components/RectangleBackground.js
@@ -9,13 +9,39 @@ import "../stylesheets/RectangleBackground.css";
 export class RectangleBackground extends Component {
   render() {
     return (
-        <div class="rectangle-background">
-          <SignInButton />
-          <LoginText />
-          <DontHaveAcctButton />
-          <ForgotPasswordButton />
-          <UsernamePassword />
-        </div>
+      <div align="center" class="rectangle-background">
+        <table class="vMiddle">
+          <tr>
+            <td>
+              <LoginText />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <UsernamePassword />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <SignInButton />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <table>
+                <tr>
+                  <td>
+                    <ForgotPasswordButton />
+                  </td>
+                  <td>
+                    <DontHaveAcctButton />
+                  </td>
+                </tr>
+              </table>
+            </td>
+          </tr>
+        </table>
+      </div>
     );
   }
 }
